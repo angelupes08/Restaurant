31a501946cac7e91e3f5bd835910fc425b98afa7
Angel <angelthegreat1998@gmail.com>
2025-01-11 19:26:53 +0530

    restaurant service added

--- src/main/java/com/restaurant/controller/RestaurantController.java
+++ src/main/java/com/restaurant/controller/RestaurantController.java
@@ -1,5 +1,7 @@
 package com.restaurant.controller;
 
+import com.restaurant.service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/restaurant")
 public class RestaurantController {
 
+    @Autowired
+    RestaurantService restaurantService;
+
     @GetMapping("")
     public String getResponse(){
 
-        return "Restaurant is opened";
+        return "Restaurant is opened"+restaurantService.restaurantService();
     }
 }
