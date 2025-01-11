4fdf118dad352bcade3e22eddf2e3f8da56928fb
Angel <angelthegreat1998@gmail.com>
2025-01-11 19:21:00 +0530

    Restaurant controller added

--- src/main/java/com/restaurant/controller/RestaurantController.java
+++ src/main/java/com/restaurant/controller/RestaurantController.java
@@ -1,4 +1,16 @@
 package com.restaurant.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/restaurant")
 public class RestaurantController {
+
+    @GetMapping("")
+    public String getResponse(){
+
+        return "Restaurant is opened";
+    }
 }
